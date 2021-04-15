bf1f32d21fb16f40bf95b1e74524e53b8f18eb7a
Mahedi Hassan <mahedihassan8855@gmail.com>
2021-04-15 13:28:48 +0600

    commits an error

--- color_detection/main.py
+++ color_detection/main.py
@@ -45,3 +45,5 @@ while True:
         break
 
 
+cap.release()
+cv.destroyAllWindows()
--- shape_detection/main.py
+++ shape_detection/main.py
@@ -7,13 +7,45 @@ frameheight=280
 cap=cv.VideoCapture(0)
 cap.set(3,framewidth)
 cap.set(4,frameheight)
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv.cvtColor( imgArray[x][y], cv.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv.cvtColor(imgArray[x], cv.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
 
 
 while True:
     success, img=cap.read()
 
-
-    cv.imshow("Result",img)
+    imgblur=cv.GaussianBlur(img,(7,7),1)
+    imggray=cv.cvtColor(imgblur,cv.COLOR_BGR2GRAY)
+    imgstack=stackImages(0.8,([img,imgblur,imggray]))
+    cv.imshow("Result",imgstack)
     if cv.waitKey(1) & 0xff==ord("f"):
         break
